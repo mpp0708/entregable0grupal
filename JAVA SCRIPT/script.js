0aa8fbb4d9d33edec380259995468035c92ad7a0
Milagros <mili1997p@gmail.com>
2022-08-04 20:59:55 -0300

    cambios en java

--- JAVA SCRIPT/script.js
+++ JAVA SCRIPT/script.js
@@ -1,7 +1,9 @@
 alert("Registro")
 
-// función para capturar datos desde un cuadro de texto // 
-function datos(){
-    var nombre =document.getElementById("nombres").value;
-    alert("nombre");
+function clickTitulo (){
+    console.log("El usuario hizo click");
+
 }
+
+const tituloform = document.getElementById("titulo");
+tituloform.addEventListener("click", clickTitulo);
