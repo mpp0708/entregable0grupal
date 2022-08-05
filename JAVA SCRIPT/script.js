bdc984249d018307c2cd71f65f42446c38750899
Milagros <mili1997p@gmail.com>
2022-08-04 21:09:48 -0300

    cambio

--- JAVA SCRIPT/script.js
+++ JAVA SCRIPT/script.js
@@ -1,9 +1,2 @@
 alert("Registro")
 
-function clickTitulo (){
-    console.log("El usuario hizo click");
-
-}
-
-const tituloform = document.getElementById("titulo");
-tituloform.addEventListener("click", clickTitulo);
